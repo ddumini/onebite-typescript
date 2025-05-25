491fbb8655cb229f51ab5b592c44f10d3c82f12f
sumin <tnals2565@naver.com>
2025-05-25 20:56:23 +0900

    study: mapped types

--- section8/src/chapter3.ts
+++ section8/src/chapter3.ts
@@ -1,4 +1,5 @@
 // 맵드 타입
+// interface로는 활용 불가능, type별칭으로 사용
 
 interface User {
   id: number;
@@ -19,15 +20,16 @@ type ReadonlyUser = {
 }
 
 // 한명의 유저 정보를 불러오는 기능
-function fetchUser(): User {
+function fetchUser(): ReadonlyUser {
   // ... 기능
   return {
     id: 1,
     name: '김수민',
-    age: 30
-  }
+    age: 30,
+  };
 }
 const user = fetchUser();
+// user.id = 1; readonly라서 불가능
 
 // 한명의 유저 정보를 수정하는 기능
 function updateUser(user: PartialUser) {
