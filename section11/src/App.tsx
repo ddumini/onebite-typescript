97209cdc2047e19a40db01d7b6de3984fe405739
sumin <tnals2565@naver.com>
2025-05-29 22:43:12 +0900

    study: state and props

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,8 +1,29 @@
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
+import Editor from './components/Editor';
+interface Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const idRef = useRef(0);
 
-  return <div className='App'></div>;
+  const onClickAdd = (text: string) => {
+    setTodos([...todos, { id: idRef.current++, content: text }]);
+  }
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd} />
+    </div>
+  );
 }
 
 export default App;
